e3a0a610de62438088bb7ae3ce1330f145cd0f90
Banee Ishaque K <Baneeishaque@gmail.com>
2025-10-15 15:27:52 +0530

    Time: 1 ms (96.79%) | Memory: 45.2 MB (84.25%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -4,14 +4,7 @@ class Solution {
         int[] ans = new int[2 * (nums.length)];
         for (int i = 0; i < (ans.length); i++) {
 
-            if (i < (nums.length)) {
-
-                ans[i] = nums[i];
-
-            } else {
-
-                ans[i] = nums[i % (nums.length)];
-            }
+            ans[i] = nums[i % (nums.length)];
         }
 
         return ans;
