c95ee2badb37452ef0f9834faf8a32f708690415
Banee Ishaque K <Baneeishaque@gmail.com>
2025-11-22 16:40:11 +0530

    Time: 1 ms (15.05%) | Memory: 43.9 MB (20.53%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        
+        int count = 0;
+        
+        for (int h : hours) {
+
+            if (h >= target) {
+
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
