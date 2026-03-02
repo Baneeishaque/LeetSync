156c8d17f52221f538e01c11baef0d99b0395972
Banee Ishaque K <Baneeishaque@gmail.com>
2026-03-02 21:25:15 +0530

    Time: 37 ms (25.64%) | Memory: 58.9 MB (92.20%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        
+        // Step 1: Sort the array
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];   // We need nums[left] + nums[right] == -nums[i]
+            
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                
+                if (sum == target) {
+                    // Found a valid triplet
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } 
+                else if (sum < target) {
+                    left++;   // need larger sum
+                } 
+                else {
+                    right--;  // need smaller sum
+                }
+            }
+        }
+        return result;
+    }
+}
